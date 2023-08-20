7da42ffc3f75264317f5bd42d4e26ca0384dafca
Rafael <142061101+rafael-iftm@users.noreply.github.com>
2023-08-20 20:31:26 -0300

    feat: ajustando a aplicacao 01

--- aplicacao_01/script.js
+++ aplicacao_01/script.js
@@ -4,8 +4,9 @@ Criar uma página web para solicitar o nome completo de uma pessoa e exibir no c
 1- Quantos caracteres tem o nome
 2- A primeira letra do nome
 3- O nome convertido para letras maisuculas
-4- O nome na cor vermelha
-5- O nome escrito 5 vezes na cor azul (Utilizar um for e colocar os casos em uma lista)
+4- A quantidade de palavras do nome
+5- O nome na cor vermelha
+6- O nome escrito 5 vezes na cor azul (Utilizar um for e colocar os casos em uma lista)
 
 Utilizando apenas pront() e write() - JavaScript
  */
@@ -22,6 +23,11 @@ primeiraLetra = nomeCompleto.charAt(0);
 // Converte o nome para letras maiúsculas
 nomeMaiusculo = nomeCompleto.toUpperCase();
 
+// Calcula a quantidade de palavras no nome
+var numPalavras = nomeCompleto.split(' ').filter(function (word) {
+    return word.trim() !== '';
+  }).length;
+
 // Cria o nome na cor vermelha
 nomeVermelho = "<span style='color: red;'>" + nomeCompleto + "</span>";
 
@@ -34,10 +40,11 @@ for (i = 0; i < 5; i++) {
 listaNomesAzuis += "</ul>";
 
 // Exibe as informações na página
-document.write("<p>1- O nome tem " + numCaracteres + " caracteres.</p>");
-document.write("<p>2- A primeira letra do nome é: " + primeiraLetra + ".</p>");
-document.write("<p>3- O nome em letras maiúsculas é: " + nomeMaiusculo + ".</p>");
-document.write("<p>4- O nome em vermelho é: " + nomeVermelho + ".</p>");
-document.write("<p>5- O nome escrito 5 vezes em azul:</p>");
+document.write("<p>O nome tem " + numCaracteres + " caracteres.</p>");
+document.write("<p>A primeira letra do nome é: " + primeiraLetra + ".</p>");
+document.write("<p>O nome em letras maiúsculas é: " + nomeMaiusculo + ".</p>");
+document.write("<p>A quantidade de palavras no nome é " + numPalavras + ".</p>");
+document.write("<p>O nome em vermelho é: " + nomeVermelho + ".</p>");
+document.write("<p>O nome escrito 5 vezes em azul:</p>");
 document.write(listaNomesAzuis);
 
